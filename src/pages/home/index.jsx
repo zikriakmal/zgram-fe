a9b9d535aae2c75d02d9962f92c6596d73a87f93
zikri <zikriakmale@gmail.com>
2022-12-15 23:13:26 +0700

    adjustment at homepage little

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,10 +5,14 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline decoration-sky-600">
-        Hello this is zgram.zikriakmal.my.id
+        Hello <br /> this is zgram.zikriakmal.my.id
       </h1>
       <Gap height={20} />
-      <button className={"rounded-lg bg-indigo-500 p-3 hover:text-red-800 hover:bg-black"}>
+      <button
+        className={
+          "rounded-lg bg-indigo-500 p-3 hover:text-red-800 hover:bg-black"
+        }
+      >
         Sebentar lagi akan launching !
       </button>
     </div>
